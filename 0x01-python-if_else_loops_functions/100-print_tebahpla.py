ec2164544f933632ac49e49acc7c97599037bfbe
heknowk <engidazer@gmail.com>
2023-11-28 08:51:07 -0800

    13

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+#!/usr/bin/python3
+for letter in range(ord('z'), ord('a') - 1, -2):
+    print('{:c}{:c}'.format(letter, letter - 33), end='')
